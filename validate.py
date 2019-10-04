7d127399424f392808e482c3638e0242a184f462
tchu5246 <32600719+tchu5246@users.noreply.github.com>
2019-10-03 20:38:30 -0400

    Create valid.py
    Create class for storing validation/testing methods

---
+++ validate.py
@@ -0,0 +1,14 @@
+#define testing functions here
+#each testing function should take in an entire Gedcom and iterate over the individuals/families
+#it will remove any entries that fail its test and print a message identifying the individual/family id
+#ex. 
+#def birth_before_death(gc):
+#   for indiv in gc.individuals:
+#       ...
+
+
+
+def validate(gc):
+  #test01(gc)
+  #test02(gc)
+  #...
