a8460fbfdb90893103d5e2ef1890cd93af352ed4
ZimingZhang957 <55264433+ZimingZhang957@users.noreply.github.com>
2019-10-10 18:20:39 -0400

    US05

--- US05_test.py
+++ US05_test.py
@@ -9,11 +9,11 @@ import validate
 class Testing(unittest.TestCase):
     # A successful case
     def test1(self):
-        gc = Parser.Gedcom(["US05a.ged"])
+        gc = Parser.Gedcom(["gedcomfile.ged"])
         self.assertEqual(validate.MarriageBeforeDeath(gc),1)
     # A failure case    
     def test2(self):
-        gc = Parser.Gedcom(["US05b.ged"])
+        gc = Parser.Gedcom(["bad_gedcomfile.ged"])
         self.assertEqual(validate.MarriageBeforeDeath(gc),0)
 
         
