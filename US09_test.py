d1489ad458c6c753d6b524763afd9866073f1f93
Pho3n1x13 <49891687+Pho3n1x13@users.noreply.github.com>
2019-10-10 19:33:51 -0400

    Update US09_test.py

--- US09_test.py
+++ US09_test.py
@@ -2,15 +2,15 @@ import unittest
 import Parser
 import validate
 
-class TestUS09(unittest.TestCase):
+class TestUS22(unittest.TestCase):
     # A successful case
-    def test_US09(self):
-        gc = Parser.Gedcom(["US09a.ged"])
+    def test_US22(self):
+        gc = Parser.Gedcom(["gedcomfile.ged"])
         self.assertEqual(validate.duplicateID(gc), 1)
     # A failure case
-    def test_US09(self):
-        gc = Parser.Gedcom(["US09b.ged"])
-        self.assertEqual(validate.duplicateID(gc), 1)
+    def test_US22(self):
+        gc = Parser.Gedcom(["bad_gedcomfile.ged"])
+        self.assertEqual(validate.duplicateID(gc), 0)
 
 
 unittest.main()
