771112b920c964ebcb37ad9104239273a2eb20be
tchu5246 <32600719+tchu5246@users.noreply.github.com>
2019-10-10 19:03:14 -0400

    Update US07_test.py

--- US07_test.py
+++ US07_test.py
@@ -4,11 +4,11 @@ import validate
 
 class TestUS07(unittest.TestCase):
     # A successful case
-    def test_US02(self):
+    def test_US07(self):
         gc = Parser.Gedcom(["US07a.ged"])
         self.assertEqual(validate.under150(gc), 1)
     # A failure case
-    def test_US02(self):
+    def test_US07(self):
         gc = Parser.Gedcom(["US07b.ged"])
         self.assertEqual(validate.under150(gc), 1)
 
