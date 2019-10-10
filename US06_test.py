8371840ed206b577f7ca1167f811805caa1489be
ZimingZhang957 <55264433+ZimingZhang957@users.noreply.github.com>
2019-10-09 23:25:02 -0400

    US06

---
+++ US06_test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@author: zzhan
+"""
+
+import unittest
+import Parser
+import validate
+
+class Testing(unittest.TestCase):
+
+    def test1(self):
+        gc = Parser.Gedcom("US06a.ged")
+        self.assertEqual(validate.DivorceBeforeDeath(gc),1)
+
+        
+    def test2(self):
+        gc = Parser.Gedcom("US06b.ged")
+        self.assertEqual(validate.DivorceBeforeDeath(gc),0)
+    
+
+        
+
+unittest.main()
