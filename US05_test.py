cceff8067432fa0b838719692f0491dfff72d7c0
ZimingZhang957 <55264433+ZimingZhang957@users.noreply.github.com>
2019-10-09 23:20:11 -0400

    Add files via upload
    US05

---
+++ US05_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@author: zzhan
+"""
+
+import unittest
+import Parser
+import validate
+
+class Testing(unittest.TestCase):
+
+    def test1(self):
+        gc = Parser.Gedcom("US05a.ged")
+        self.assertEqual(validate.MarriageBeforeDeath(gc),1)
+        
+    def test2(self):
+        gc = Parser.Gedcom("US05b.ged")
+        self.assertEqual(validate.MarriageBeforeDeath(gc),0)    
+
+        
+
+    
+
+        
+
+unittest.main()
