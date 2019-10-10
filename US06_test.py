8efbc3b5db2e9f9e83a7b59dc2f46f3f028fbbbe
ZimingZhang957 <55264433+ZimingZhang957@users.noreply.github.com>
2019-10-09 23:34:00 -0400

    US06

--- US06_test.py
+++ US06_test.py
@@ -7,12 +7,12 @@ import Parser
 import validate
 
 class Testing(unittest.TestCase):
-
+    # A success case
     def test1(self):
         gc = Parser.Gedcom("US06a.ged")
         self.assertEqual(validate.DivorceBeforeDeath(gc),1)
 
-        
+    # A failure case    
     def test2(self):
         gc = Parser.Gedcom("US06b.ged")
         self.assertEqual(validate.DivorceBeforeDeath(gc),0)
