213497580ab5b9cbb2808c816d8181ba59a0a361
Pho3n1x13 <49891687+Pho3n1x13@users.noreply.github.com>
2019-10-10 19:49:15 -0400

    Update US04_test.py

--- US04_test.py
+++ US04_test.py
@@ -4,11 +4,11 @@ import validate
 
 class TestUS04(unittest.TestCase):
     # A successful case
-    def test_US04(self):
+    def test1(self):
         gc = Parser.Gedcom(["gedcomfile.ged"])
         self.assertEqual(validate.divBeforeMarr(gc), 1)
     # A failure case
-    def test_US04(self):
+    def test2(self):
         gc = Parser.Gedcom(["bad_gedcomfile.ged"])
         self.assertEqual(validate.divBeforeMarr(gc), 0)
 
