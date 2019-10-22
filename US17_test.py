ad6f3e109810ba60020ba546a07a9afbcb38c304
ZimingZhang957 <zzhan42@stevens.edu>
2019-10-22 17:06:48 -0400

    US17

---
+++ US17_test.py
@@ -0,0 +1,25 @@
+"""
+@author: ziming
+"""
+
+import unittest
+import Parser
+import validate
+
+class Testing(unittest.TestCase):
+    # A successful case
+    def test1(self):
+        gc = Parser.Gedcom(["gedcomfile.ged"])
+        self.assertEqual(validate.NoMarriagesToDescendants(gc),1)
+        
+    # A failure case    
+    def test2(self):
+        gc = Parser.Gedcom(["bad_gedcomfile.ged"])
+        self.assertEqual(validate.NoMarriagesToDescendants(gc),0)
+
+        
+
+    
+
+        
+unittest.main()
