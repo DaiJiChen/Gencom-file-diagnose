292413c2f42b29b78a63e4c154e21b3e32cb6cc9
tchu5246 <32600719+tchu5246@users.noreply.github.com>
2019-10-23 16:53:18 -0400

    Create US21_test.py

---
+++ US21_test.py
@@ -0,0 +1,16 @@
+import unittest
+import Parser
+import validate
+
+class TestUS16(unittest.TestCase):
+    # A successful case
+    def test_US16(self):
+        gc = Parser.Gedcom(["gedcomfile.ged"])
+        self.assertEqual(validate.roleGender(gc), 1)
+    # A failure case
+    def test_US16(self):
+        gc = Parser.Gedcom(["bad_gedcomfile.ged"])
+        self.assertEqual(validate.roleGender(gc), 0)
+
+
+unittest.main()
